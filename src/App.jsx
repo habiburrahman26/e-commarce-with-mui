be55880313e3a2da0bdb488180cbd2007ce47741
HabiburRahmanAnik <habib264521@gmail.com>
2023-04-12 08:44:18 +0600

    app bar, navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { CssBaseline } from '@mui/material';
+import AppBarSection from './components/AppBarSection';
 import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div>
+      <CssBaseline/>
+      <AppBarSection />
       <Navbar />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,109 +1,99 @@
 import {
   Box,
-  Button,
   Container,
   Divider,
   IconButton,
-  InputBase,
+  Link,
   Stack,
   Typography,
 } from '@mui/material';
-import React from 'react';
-import LocalMallIcon from '@mui/icons-material/LocalMall';
-import PersonIcon from '@mui/icons-material/Person';
-import MessageIcon from '@mui/icons-material/Message';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MenuIcon from '@mui/icons-material/Menu';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const Navbar = () => {
   return (
-    <Container maxWidth="lg">
-      <Box
-        py={2}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}
-      >
-        <Stack direction="row" alignItems="center" spacing={1}>
-          <Box
-            sx={{
-              bgcolor: '#0D6EFD',
-              width: '40px',
-              height: '40px',
-              borderRadius: 2,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-
-              '& svg': {
-                fontSize: '24px',
-                color: '#8CB7F5',
-              },
-            }}
-          >
-            <LocalMallIcon />
-          </Box>
-          <Typography variant="h5" fontWeight={700} color="#8CB7F5">
-            Brand
-          </Typography>
-        </Stack>
-        <Box
-          sx={{
-            border: '2px solid #0D6EFD',
-            borderRadius: '6px',
-            width: '50%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
-          <InputBase
-            fullWidth
-            type="text"
-            placeholder="Search..."
-            sx={{ px: 2 }}
-          />
-          <Button
-            variant="contained"
-            bgcolor="#0D6EFD"
-            sx={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
-          >
-            Search
-          </Button>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            gap: 4,
-
-            '& .MuiStack-root': {
-              cursor: 'pointer',
-              color: '#8B96A5',
-            },
-          }}
-        >
-          <Stack alignItems="center">
-            <PersonIcon />
-            <Typography variant="body2">Profile</Typography>
-          </Stack>
-          <Stack alignItems="center">
-            <MessageIcon />
-            <Typography variant="body2">Message</Typography>
-          </Stack>
-          <Stack alignItems="center">
-            <FavoriteIcon />
-            <Typography variant="body2">Orders</Typography>
-          </Stack>
-          <Stack alignItems="center">
-            <ShoppingCartIcon />
-            <Typography variant="body2">My Cart</Typography>
-          </Stack>
-        </Box>
-      </Box>
+    <Box>
       <Divider />
-    </Container>
+      <Container maxWidth="lg">
+        <Stack
+          py={2}
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Stack direction="row" alignItems="center" gap={3}>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: 0.5,
+              }}
+            >
+              <MenuIcon />
+              <Link variant="body2" href="#" underline="none" color="#000">
+                All Category
+              </Link>
+            </Box>
+            <Link variant="body2" href="#" underline="none" color="#000">
+              Hot offers
+            </Link>
+            <Link variant="body2" href="#" underline="none" color="#000">
+              Gift boxes
+            </Link>
+            <Link variant="body2" href="#" underline="none" color="#000">
+              Projects
+            </Link>
+            <Link variant="body2" href="#" underline="none" color="#000">
+              Menu item
+            </Link>
+            <Box>
+              <Typography variant="body2">Help</Typography>
+            </Box>
+          </Stack>
+          <Stack component="div" direction="row" alignItems="center" gap={3}>
+            <Stack direction="row" alignItems="center">
+              <Typography variant="body2">English, </Typography>
+              <Stack direction="row" alignItems="center">
+                <Typography variant="body2" textTransform="uppercase">
+                  USD{' '}
+                </Typography>
+                <IconButton
+                  size="small"
+                  sx={{
+                    '& svg': {
+                      color: '#8B96A5',
+                    },
+                  }}
+                >
+                  <KeyboardArrowDownIcon />
+                </IconButton>
+              </Stack>
+            </Stack>
+            <Stack direction="row" alignItems="center">
+              <Typography variant="body2">Ship to </Typography>
+              <Stack direction="row" alignItems="center">
+                <img
+                  src="https://www.giz.de/static/en/media/Bangladesh_flag.png"
+                  alt="bd image"
+                  style={{ height: '25px' }}
+                />
+                <IconButton
+                  size="small"
+                  sx={{
+                    '& svg': {
+                      color: '#8B96A5',
+                    },
+                  }}
+                >
+                  <KeyboardArrowDownIcon />
+                </IconButton>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Container>
+      <Divider />
+    </Box>
   );
 };
 
